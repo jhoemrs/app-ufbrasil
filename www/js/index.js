b98e9d326b91e93b541380d0b2011302ca1d2eb4
Jhonatan Mark <jhoemrs@gmail.com>
2015-03-03 19:26:55 -0300

    Aplicativo V.1.0

--- www/js/index.js
+++ www/js/index.js
@@ -1,22 +1,38 @@
     var url = "http://127.0.0.1:8000/";
-    
+    var acron = null;
     $(document).ready(function() {
-    $.ajax({
-        type: 'GET',
-        url: url+"listar-estados",
-        dataType: 'json',
-        contentType: 'application/json',
-        crossDomain: true,
-        cache: false,
-        sucess: function(data)
-        {
-            $.each(data, function(i,estado){
-                item = "<option value=" + estado.nome + ">" + estado.nome + "</option>";
-                $('#selecaoEstado').append(item);
-            });
-        },
-        error: function(jqXHR, textStatus, errorThrow){
-            alert('Erro ao carregar os Estados');
-        }
-    });
+        $.ajax({
+            type: 'GET',
+            url: url+"listar-estados",
+            dataType: 'json',
+            contentType: 'application/json',
+            crossDomain: true,
+            cache: false,
+            success: function(data)
+            {
+                acron = data;
+                $('#selecaoEstado').append("<option value=0> Não Selecionado </option>");
+                $.each(data, function(i,estado){
+                    item = "<option value=" + estado.id + ">" + estado.nome + "</option>";
+                    $('#selecaoEstado').append(item);
+                    //acron.push(estado.acronimo);
+                });
+            },
+            error: function(jqXHR, textStatus, errorThrow){
+                alert('Erro ao carregar os Estados');
+            }
+        });
 });
+
+$('#selecaoEstado').on('change', function() {    
+    $.each(acron,function(x,acr){
+        if( $('#selecaoEstado').val() == 0 ){
+            $('#acronimoEstado').attr('placeholder','Não Selecionado');
+        }
+        else if( $('#selecaoEstado').val() == acr.id ){
+            $('#acronimoEstado').attr('placeholder',acr.acronimo);
+        }
+    })
+}); 
+
+                    
